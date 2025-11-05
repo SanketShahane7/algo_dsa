c89e5cb841700b35f010958b8ab2c93988e433a6
SanketShahane8 <sanket.shahane@outlook.com>
2025-11-05 19:34:50 +0530

    Check Dupliates in Array

---
+++ Array/Duplicates/ArrayHashing-Duplicates.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace algo_dsa.Array.Duplicates;
+public class ArrayHashingDuplicates
+{
+    public bool HasDuplicate(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+
+        foreach (int num in nums)
+        {
+            if (seen.Contains(num))
+                return true; // Duplicate found
+            seen.Add(num);
+        }
+
+        return false; // No duplicates found
+    }
+}
